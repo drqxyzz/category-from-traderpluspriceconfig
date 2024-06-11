e4ccb520fd352ebf4b35600330d0b95303e642b3
drq <117029096+drqxyzz@users.noreply.github.com>
2024-06-11 18:45:10 +0000

    Update category.py

--- category.py
+++ category.py
@@ -4,10 +4,8 @@ def extract_category_names(input_file, output_file):
     with open(input_file, 'r') as f:
         content = f.read()
 
-    # Use regular expression to find all occurrences of "CategoryName"
     categories = re.findall(r'"CategoryName":\s*"([^"]+)"', content)
 
-    # Convert category names to the desired format
     formatted_categories = ',\n'.join([f'\t\t"{category}"' for category in categories])
 
     with open(output_file, 'w') as f:
@@ -15,10 +13,8 @@ def extract_category_names(input_file, output_file):
 
     print(f"Category names extracted and saved to {output_file}")
 
-# Replace 'input_file.json' with the path to your input file
 input_file = 'TraderPlusPriceConfig.json'
 
-# Replace 'output_file.txt' with the desired output file name
 output_file = 'TraderPlusPriceConfig.txt'
 
 extract_category_names(input_file, output_file)
